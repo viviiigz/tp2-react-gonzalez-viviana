5e7e1c088c69ef011e2ef5ba0a8af8bc3521eefa
Viviana Gonzalez <vivigz06@gmail.com>
2026-04-06 09:37:13 -0300

    first init

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const PORT = 3000;
 
 app.use(express.json());
 
-let donaciones = [
-  { id: 1, producto: "Cajones de manzanas", cantidad: 5, estado: "disponible" },
-  { id: 2, producto: "Panificados varios", cantidad: 20, estado: "reservado" }
-];
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
